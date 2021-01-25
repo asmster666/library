aa345d746b32837e9ff5abb438495ec40671f202
uscracken <colfarrell@yandex.ru>
2021-01-25 12:44:27 +0500

    delete buttons, show only last one

--- src/components/book_card.vue
+++ src/components/book_card.vue
@@ -5,69 +5,60 @@
         >
             <a>id/ISBN</a>
             <h1>Name of Book</h1>
-            <div class="wrapper">
-                <p id="author">author</p>
+            <div class="wrapp">
+                <p id="authr">author</p>
                 <p id="categ">category</p>
             </div>
-            <span>{{ description }}</span>
+            <div id="descr">description</div>
         </router-link>
     </div>
 </template>
 
 <script>
-    import json from '../../database.json'
-
     export default {
-        name: 'Card',
-        data() {
-            return {
-                data: Object.assign({}, json),
-                description: "descr"
-            }
-        },
+        name: 'Card'
     }
 </script>
 
 <style lang="scss" scoped>
     .card {
-        width: 20rem;
-        height: 20rem;
+        width: 40%;
+        height: auto;
+        margin: 1rem auto;
         border: 0.1rem solid blue;
         background-color: beige;
         display: flex;
         flex-direction: column;
     }
-        a {
-            color: green;
+    a {
+        color: green;
+        margin: 0.3rem;
+    }
+    h1 {
+        color: blue;
+        font-weight: 600;    
+    }
+    .wrapp {
+        display: flex;
+        flex-direction: column;
+    }
+    #authr {
+        height: 2rem;
+        display: flex;
+        justify-content: flex-start;
+        margin-left: 3rem;
+        margin-top: -0.5rem;
+    }
+    #categ {
             display: flex;
             justify-content: flex-start;
-            margin: 0.3rem;
+            margin-left: 6rem;
+            margin-top: -0.5rem;
         }
-        h1 {
-            color: blue;
-            font-weight: 600;    
-        }
-        .wrapper {
-            display: flex;
-            flex-direction: column;
-        }
-            #author {
-                height: 2rem;
-                display: flex;
-                justify-content: flex-start;
-                margin-left: 3rem;
-                margin-top: -0.5rem;
-            }
-            #categ {
-                display: flex;
-                justify-content: flex-start;
-                margin-left: 6rem;
-                margin-top: -0.5rem;
-            }
-        span {
-            width: 80%;
-            height: 8rem;
-            border: 1px solid black;
-            margin: 0.5rem auto;
-        }
+    #descr {
+        width: 80%;
+        height: 8rem;
+        border: 1px solid black;
+        margin: 0.5rem auto;
+    }
 </style>
--- src/components/list_tree.vue
+++ src/components/list_tree.vue
@@ -2,13 +2,62 @@
     <ul class="category_tree">
         <li 
             v-for="category in db.category"
-            :key="index=category.id"
+            :key="category.id"
+            @change="addToCategory($this)"
             class="category"
         >
-            <h4>{{ category.name }}</h4>
-            <div class="button_wrap">
-                <button class="btn btn-primary">edit</button>
-                <button @click="$delete(this.category)" class="btn btn-secondary">delete</button>
+            <div class="wrapper">
+                <div  
+                    class="title"
+                    @click="showItem"
+                >
+                    <h3>{{ category.name }}</h3>
+                </div>
+                <div class="button_wrap">
+                    <button @click="editField($event)" class="btn btn-outline-info">edit</button>
+                    <button @click="deleteCategory($event)" class="btn btn-outline-warning">delete</button>
+                </div>
+            </div>
+            <div 
+                class="subcategories"
+                ref='genre'
+            >
+                <ul>
+                    <li 
+                        v-for="genre in category.genres" :key="genre.id"
+                        class="genre"
+                    >
+                        <div 
+                            class="genre_wrap"
+                        >
+                            <h4>{{ genre.name }}</h4>
+                            <div class="button_wrap">
+                                <button @click="editField($event)" class="btn btn-outline-info">edit</button>
+                                <button @click="deleteCategory($event)" class="btn btn-outline-warning">delete</button>
+                            </div>
+                        </div>
+                        <div class="books"> 
+                            <ul>
+                                <li
+                                    v-for="book in genre.books"
+                                    :key="book.id"
+                                    class="book"
+                                >
+                                    <div>
+                                        <h5>{{ book.name }}</h5>
+                                        <div id="author">{{ book.author }}</div>
+                                        <div id="isbn">{{ book.id }}</div>
+                                        <p id="descr">{{ book.description }}</p>
+                                    </div>
+                                    <div class="button_wrap2">
+                                        <button @click="editField($event)" class="btn btn-outline-info">edit</button>
+                                        <button @click="deleteBook($event)" class="btn btn-outline-warning">delete</button>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
             </div>
         </li>
     </ul>
@@ -19,17 +68,38 @@
 
     export default {
         name: 'Tree', 
-        props: {
-            category: String
-        },
         data() {
             return {
-                db: Object.assign({}, json),
-                categories: []
+                db: Object.assign({}, json)
             } 
         },
-        mounted() {
-            
+        methods: {
+            addToCategory(item) {
+                this.subcategories.push(item);
+                console.log(item);
+            },
+
+            showItem() {
+                let item = this.$refs.genre;
+                console.log(item);
+                item.classList.add('active');
+            },
+
+            editField(event) {
+                let parent = event.target.parentNode.parentNode;
+                let kid = parent.firstChild;
+                console.log(kid);
+            },
+
+            deleteCategory(event){
+                let field = event.target.parentElement.parentNode.parentNode;
+                field.remove();
+            },
+
+            deleteBook(event) {
+                let field = event.target.parentElement.parentNode;
+                field.remove();
+            }
         }
     }
 </script>
@@ -45,18 +115,96 @@
     }
     .category {
         width: 95%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .wrapper {
+        width: 100%;
         height: 4rem;
         display: flex;
         justify-content: space-between;
         align-items: center;
         border: 1px solid black;
     }
+    .subcategories {
+        width: 90%;
+        height: auto;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        background-color: beige;
+        display: none;
+    }
+    .title {
+        width: 80%;
+    }
     h4 {
-        margin-left: 3rem;
+        width: 50%;
+        margin-top: 1rem;
+        margin-left: 2rem;
+        text-align: left;
     }
     .button_wrap {
         width: 20%;
         display: flex;
         justify-content: space-evenly;
+    }
+    h3 {
+        text-transform: uppercase;
+        margin-left: 2rem;
+        text-align: left;
+    }
+    .genre {
+        margin-left: -2.5rem;
+        list-style: none;
+        border: 1px solid black;
+    }
+    .genre_wrap {
+        display: flex;
+        justify-content: space-between;
+    }
+    .active {
+        display: block;
+    }
+    .books {
+        width: 90%;
+        margin-top: 2rem;
+        height: auto;
+        display: flex;
+        background-color: pink;
+    }
+    .book {
+        width: 100%;
+        margin-left: -1.2rem;
+        margin-top: 1rem;
+        list-style: none;
+        display: flex;
+    }
+    h5 {
+        font-weight: bold;
+        text-align: left;
+        margin-top: 1rem;
+    }
+    #author {
+        text-align: left;
+        font-family: cursive;
+    }
+    #isbn {
+        display: none;
+    }
+    #descr {
+        overflow-y: scroll;
+        overflow: hidden;
+        height: 4.5rem;
+    }
+    .button_wrap2 {
+        height: auto;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        width: 30%;
+        margin: 0.5rem;
     }
 </style>
